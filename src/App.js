17a77dfe501e480a28a1acfa9e07c4a565d7f542
Priyanka Mukund <priyanka@kunday.com>
2019-03-06 12:43:26 +1100

    added add cheese fn

--- src/App.js
+++ src/App.js
@@ -10,16 +10,26 @@ class App extends Component {
       stack: ['bun', 'cheese', 'cheese', 'bun']
     }
   }
+
   addBun = () => {
     // requirement from react =  when you update state 
     // you need to update it without mutation
 
     // var newStack = this.state.stack.slice(0);
     // newStack.push('bun')
+    // var a = [1, 2, 3]
+    // spread syntax
+    // var b = [...a] // b = a.slice(0)
+    // var newStack = [...this.state.stack]
 
-    
     this.setState({
-      stack: newStack
+      stack: [...this.state.stack, "bun"]
+    })
+  }
+
+  addCheese = () => {
+    this.setState({
+      stack: [...this.state.stack, "cheese"]
     })
   }
 
@@ -29,7 +39,7 @@ class App extends Component {
       <div className="App">
         <aside>
           <button onClick={this.addBun}>Bun</button>
-          <button>Cheese</button>
+          <button onClick={this.addCheese}>Cheese</button>
         </aside>
 
         <main>
